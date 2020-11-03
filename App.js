c5cbf2871daf1a0e1d17b846edb79b3a172d9bdb
Vishal Sengar <vishalsengardtu@gmail.com>
2020-11-03 16:12:22 +0530

    Lists and ScrollView

--- App.js
+++ App.js
@@ -1,44 +1,47 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('Vishal');
-  const [age, setAge] = useState(21);
-  
+  const [people, setPeople] = useState([
+    { name: 'shaun', key: '1' },
+    { name: 'yoshi', key: '2' },
+    { name: 'mario', key: '3' },
+    { name: 'luigi', key: '4' },
+    { name: 'peach', key: '5' },
+    { name: 'toad', key: '6' },
+    { name: 'bowser', key: '7' },
+    { name: 'ronaldo', key: '8' },
+  ]);
+
   return (
-    <View style={styles.container}> 
-      <Text>Enter Name:</Text>
-      <TextInput
-        multiline
-        style={styles.input}
-        placeholder='Enter Name'
-        onChangeText={(val) => setName(val)} 
-      />
-      <Text>Enter Age:</Text>
-      <TextInput
-        keyboardType='numeric'
-        style={styles.input}
-        placeholder='Enter Age'
-        onChangeText={(val) => setAge(val)} 
-      />
-      <Text>Name: {name}, Age: {age}</Text>
+    <View style={styles.container}>
+      <ScrollView>
+        {
+          people.map(item => (
+              <View key={item.key}>
+                <Text style={styles.item}>{item.name}</Text>
+              </View>
+            )
+          )
+        }
+        </ScrollView>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width: 200,
-    textAlign: 'center',
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24,
   }
 });
