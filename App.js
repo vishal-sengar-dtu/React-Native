f9935d3dd661b2536133a009ea81a1314ecb68e2
Vishal Sengar <vishalsengardtu@gmail.com>
2020-11-05 00:13:12 +0530

    TouchableOpacity

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
   const [people, setPeople] = useState([
@@ -13,6 +13,13 @@ export default function App() {
     { name: 'ronaldo', id: '8' },
   ]);
 
+  const pressHandler = (id) => {
+    console.log(id);
+    setPeople((prev) => {
+      return prev.filter((person) => person.id != id)
+    })
+  }
+
   return (
     <View style={styles.container}>
       
@@ -20,17 +27,11 @@ export default function App() {
         keyExtractor={(item) => item.id}
         data={people}
         renderItem={({ item }) => (
-          <Text style={styles.item}>{item.name}</Text>
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>  
         )}
       />
-
-      {/* <ScrollView>
-        {people.map(item => (
-          <View key={item.key}>
-            <Text style={styles.item}>{item.name}</Text>
-          </View>
-        ))}
-        </ScrollView> */}
     </View>
   )
 }
@@ -41,8 +42,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     paddingTop: 40,
     paddingHorizontal: 20,
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
   item: {
     marginTop: 24,
